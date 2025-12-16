aacc4ff95e85c9b26bf8b9af0e7d1324bff00894
Pineda <jjrn@gmv.com>
2025-12-16 14:56:09 +0100

    Uploading project WatchQueueMessages

---
+++ WatchQueueMessages/Program.cs
@@ -0,0 +1,19 @@
+﻿using WatchQueueMessages;
+
+var opts = WatchQueueMessagesOptions.Parse(args);
+if (opts == null)
+{
+    WatchQueueMessagesOptions.PrintHelp();
+    return;
+}
+
+try
+{
+    var app = new WatchQueueMessagesApp(opts);
+    await app.RunAsync();
+}
+catch (Exception ex)
+{
+    Console.Error.WriteLine($"Error: {ex.Message}");
+    Environment.Exit(1);
+}
